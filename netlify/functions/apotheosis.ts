4194b1a9e34ee64aea95682667d5a2d27b985391
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-01-29 22:28:46 +0400

    feat: listen to the synchronize event

--- netlify/functions/apotheosis.ts
+++ netlify/functions/apotheosis.ts
@@ -28,15 +28,34 @@ type PullAction =
   | "unlabeled"
   | "unlocked";
 
+type User = {
+  // Non-exhaustive, we just need this really
+  login: string;
+};
+
+type PullRequest = {
+  user: User;
+};
+
 type PullActivity = {
   action: PullAction;
   repository: string;
-  sender: string;
   number: number;
+  pull_request: PullRequest;
+  sender: User;
+};
+
+const yell = async () => {
+  console.log("This should yell");
+};
+
+const reopen = async () => {
+  console.log("This should reopen the PR");
 };
 
 const isMerged = async (pullNumber: number) => {
-  await new Promise((resolve) => setTimeout(resolve, 4000));
+  // Wait 30 seconds after the synchronize event is triggered to ensure the branch is merged
+  await new Promise((resolve) => setTimeout(resolve, 30000));
 
   const { data: pr } = await octokit.rest.pulls.get({
     owner: SS_OWNER,
@@ -45,6 +64,16 @@ const isMerged = async (pullNumber: number) => {
   });
   return pr.merged;
 };
+
+const isClosed = async (pullNumber: number) => {
+  const { data: pr } = await octokit.rest.pulls.get({
+    owner: SS_OWNER,
+    repo: SS_WEB_REPO,
+    pull_number: pullNumber,
+  });
+  return pr.state === "closed";
+};
+
 const handler: Handler = async (event) => {
   const okResponse = {
     statusCode: 200,
@@ -57,13 +86,22 @@ const handler: Handler = async (event) => {
 
   const pullActivity: PullActivity = JSON.parse(event.body);
 
-  const merged = await isMerged(pullActivity.number);
+  // We only want to listen to the synchronize event i.e base branch changed:
+  // "A pull request's head branch was updated. For example, the head branch was updated from the base branch or new commits were pushed to the head branch."
+  // https://docs.github.com/developers/webhooks-and-events/webhooks/webhook-events-and-payloads?actionType=synchronize#pull_request
+  if (pullActivity.action !== "synchronize") return noOpResponse;
 
-  if (
-    pullActivity.sender === "0xapotheosis" &&
-    pullActivity.action === "closed" &&
-    !merged
-  ) {
+  // PRs are automatically rebased to a new base branch when the previous branch's PR is merged
+  // This triggers a synchronize event from the PR author, which is the only event we care about
+  if (pullActivity.sender.login !== pullActivity.pull_request.user.login)
+    return noOpResponse;
+
+  const merged = await isMerged(pullActivity.number);
+  const closed = await isClosed(pullActivity.number);
+
+  if (pullActivity.sender.login === "0xapotheosis" && closed && !merged) {
+    await yell();
+    await reopen();
     return okResponse;
   }
 
